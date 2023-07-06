215678f9cfb619e4564b2dd1952518eee70942dd
blank_zs <thiru.sanjaykumar@gmail.com>
2023-07-06 15:47:51 +0530

    new

--- finalapi.mjs
+++ finalapi.mjs
@@ -3,6 +3,9 @@ import pdf2img from "pdf-img-convert";
 import fetch from "node-fetch";
 import cors from "cors";
 import crypto from "crypto";
+import dotenv from "dotenv";
+
+dotenv.config(); // Load environment variables from the .env file
 
 const app = express();
 app.use(express.json());
@@ -29,7 +32,7 @@ app.post("/convert-pdf", async (req, res) => {
 
       mainjson.push(json);
 
-      const response = await fetch("http://13.232.187.12:5000/ocr/resources", {
+      const response = await fetch(process.env.API_BASE, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -37,11 +40,14 @@ app.post("/convert-pdf", async (req, res) => {
         body: JSON.stringify({ value: json.base64Data, title: json.title }),
       });
 
+      console.log("Response:", response);
     }
 
+    console.log("-->", mainjson);
 
     res.json(mainjson);
   } catch (error) {
+    console.error("Error:", error);
     res.status(500).json({ error: "An error occurred" });
   }
 });
